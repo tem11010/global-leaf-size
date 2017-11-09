aa964def3478ec1a1d95ac4c51be84620a0f5211
James Mickley <mickley@gmail.com>
2017-11-09 17:43:10 -0500

    Cleaned up the AIC R2 table code

--- AIC.R2.table.R
+++ AIC.R2.table.R
@@ -1,3 +1,10 @@
+
+# This function constructs a table of GLMMs ordered from best to worst by AIC
+# It includes the AIC, dAIC, and marginal and conditional R^2 for each model.
+# The R^2 are calculated using methods from Nakagawa & Schielzeth 2013 and 
+# require the MuMIn package
+# Optionally, you can include a vector of names to use for the models instead 
+# of their variable names
 AIC.R2.table <- function(..., mnames) {
     
     # Get the model names if they weren't specified
@@ -10,35 +17,45 @@ AIC.R2.table <- function(..., mnames) {
         mnames <- as.character(Call)[c(-1, -xargs)]
     }
     
+    # Get a list of the models sent to the function
     models <- list(...)
+    
+    # Get the number of models
     len <- length(models)
     
-    #AICc <- MuMIn::AICc(...)
-    
-    #print(AICc)
-    
+    # Make an empty dataframe to hold the AIC/R^2 table
     tbl <- data.frame(Model = character(len), AIC = numeric(len), 
-                      R2.marg = numeric(len), R2.cond = numeric(len), 
-                      stringsAsFactors = F)
+        R2.marg = numeric(len), R2.cond = numeric(len), stringsAsFactors = F)
     
-    
+    # Calculate AIC and R^2 for each model
     for (i in 1:length(models)){
+        
+        # Store the model name
         tbl$Model[i] <- mnames[i]
+        
+        # Store the AIC
         tbl$AIC[i] <- AIC(models[[i]])
         
-        R2 <- r.squaredGLMM(models[[i]])
+        # Get the marginal and conditional R^2 using MuMIn
+        R2 <- MuMIn::r.squaredGLMM(models[[i]])
         
+        # Store the marginal and conditional R^2
         tbl$R2.marg[i] <- round(R2[1], 2)
         tbl$R2.cond[i] <- round(R2[2], 2)
     }
     
+    # Clean up the table using some dplyr
     tbl <- tbl %>%
         
+        # Order by AIC with the best model first
         arrange(AIC) %>%
         
+        # Add a column for delta AIC
         mutate(dAIC = round(AIC - min(AIC), 2)) %>%
         
+        # Reorder the columns
         select(Model, AIC, dAIC, R2.marg, R2.cond)
     
+    # Return the table
     return(tbl)
 }
